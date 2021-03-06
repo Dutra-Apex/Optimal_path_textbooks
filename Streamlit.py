0432337ed72f321b7ca6441a6c8abe9919667aa3
Gabriel Dutra <57206480+Dutra-Apex@users.noreply.github.com>
2021-03-05 23:51:02 -0500

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -181,13 +181,13 @@ while True:
     data.plot()
 
     
-    #c = alt.Chart(df).mark_circle(color='blue', opacity=0.5).encode(x='a',y='b', size='c', color='c', tooltip=['a','b','c'])
-    #st.write(c)
+    c = alt.Chart(data).mark_circle(color='blue', opacity=0.5).encode(x='a',y='b', size='c', color='c', tooltip=['a','b','c'])
+    st.write(c)
    
 
-    #df = pd.DataFrame(
-     #   np.random.randn(200, 3),
-      #  columns=['a', 'b', 'c'])
+    df = pd.DataFrame(
+        np.random.randn(200, 3),
+        columns=['a', 'b', 'c'])
     
     c = alt.Chart(df).mark_circle().encode(
         x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
